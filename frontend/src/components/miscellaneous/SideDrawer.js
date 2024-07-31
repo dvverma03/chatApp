54c1f7e257b861e5f3f865d2fc91cb198ce6c8fe
dvverma03 <dvverma9211@gmail.com>
2024-07-31 19:05:09 +0530

    updarte notification icon

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -30,7 +30,7 @@ import ChatLoading from "../ChatLoading";
 import axios from "axios";
 import UserListItem from "../UserAvatar/UserListItem";
 import { getSender } from "../../config/ChatLogics";
-import NotificationBadge, { Effect } from "react-notification-badge"
+// import NotificationBadge, { Effect } from "react-notification-badge"
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
@@ -148,10 +148,10 @@ const SideDrawer = () => {
         <div>
           <Menu>
             <MenuButton p={1}>
-              <NotificationBadge
+              {/* <NotificationBadge
               count={notification.length}
               effect={Effect.SCALE}
-              />
+              /> */}
               <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
             <MenuList pl={2}>
